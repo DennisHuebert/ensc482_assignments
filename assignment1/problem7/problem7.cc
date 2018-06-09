7bfc8df8ad94b64c210bd3847e77c0854f06ee94
dennishuebert <dennishuebert1@gmail.com>
2018-06-09 16:37:09 -0700

    finished problem 7

--- assignment1/problem7/problem7.cc
+++ assignment1/problem7/problem7.cc
@@ -31,7 +31,7 @@ void initRendering()
 {
     glClearColor(1.0, 1.0, 1.0, 0.0);
     glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(-700.0, 700.0, -700.0, 700.0);
+    gluOrtho2D(0.0, 100.0, -650.0, 500.0);
 }
 
 void drawText(const char *text, int length, int x, int y){
@@ -39,7 +39,7 @@ void drawText(const char *text, int length, int x, int y){
     double *matrix = new double[16];
     glGetDoublev(GL_PROJECTION_MATRIX, matrix);
     glLoadIdentity();
-    glOrtho(-700, 700, -700, 700, -5, 5);
+    glOrtho(0, 100, -650, 500, -5, 5);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glPushMatrix();
@@ -60,7 +60,8 @@ void drawText(const char *text, int length, int x, int y){
 
 void lineSegmentsAndText(void){
 
-    string text[] = {"0.02632", "0.97368", "Win", "Lose"};
+    string text1[] = {"q = 28.5714%", "0", "1", "2", "4", "Pay off to player one", "Moving up plays: q + 1", "Moving down plays: 4 - 3q"};
+    string text2[] = {"q = 75%", "1", "-3", "2", "-1", "Pay off to player two", "Moving left plays: 1 - 4q", "Moving right plays: 3q - 1"};
     
     glClear(GL_COLOR_BUFFER_BIT);
     
@@ -83,6 +84,32 @@ void lineSegmentsAndText(void){
             glEnd();
         }
     }
+
+    drawText(text1[0].data(), text1[0].size(), 70, 200);
+    drawText(text1[2].data(), text1[2].size(), 0, 105);
+    drawText(text1[4].data(), text1[4].size(), 0, 405);
+    drawText(text1[2].data(), text1[2].size(), 98, 107);
+    drawText(text1[3].data(), text1[3].size(), 98, 207);
+    drawText(text1[5].data(), text1[5].size(), 0, 485);
+    drawText(text1[6].data(), text1[6].size(), 37, 300);   
+    drawText(text1[7].data(), text1[7].size(), 37, 117);
+
+    drawText(text2[0].data(), text2[0].size(), 30, -300);
+    drawText(text2[1].data(), text2[1].size(), 0, -195);
+    drawText(text2[2].data(), text2[2].size(), 0, -385);
+    drawText(text2[3].data(), text2[3].size(), 98, -190);
+    drawText(text2[4].data(), text2[4].size(), 97, -587);
+    drawText(text2[5].data(), text2[5].size(), 0, -15);
+    drawText(text2[6].data(), text2[6].size(), 60, -300);
+    drawText(text2[7].data(), text2[7].size(), 60, -435);  
+
+    glColor3f(0.0, 1.0, 0.0);
+
+    glBegin(GL_LINES);
+        glVertex2i(0, 0);
+        glVertex2i(100, 0);
+    glEnd();
+
     glFlush();
 }
 
@@ -99,22 +126,12 @@ int main(int argc, char** argv){
                         GAMEMATRIX[1][0] - GAMEMATRIX[0][2] + GAMEMATRIX[1][2]);
     player2ProbRight = 1 - player2ProbLeft;
 
-    cout << "\nplayer 1 prob up: " << player1ProbUp*100 << "%\n";
-    cout << "player 1 prob down: " << player1ProbDown*100 << "%\n";
-    
-    cout << "player 2 prob left: " << player2ProbLeft*100 << "%\n";
-    cout << "player 2 prob right: " << player2ProbRight*100 << "%\n\n";
-
-    /*for(int i = 0; i < sizeof(PLAYER_ONE_EU)/sizeof(PLAYER_ONE_EU[0]); i++){
-        for(int j = 0; j < sizeof(PLAYER_ONE_EU[0])/sizeof(int); j++){
-            cout << PLAYER_ONE_EU[i][j] << ", ";
-        }
-        cout << endl;
-    }*/
+    cout << "\nPlayer 1 should play up: " << player1ProbUp*100 << "% of the time when player 2 plays left\n";
+    cout << "Player 2 should play left: " << player2ProbLeft*100 << "% of the time when player 1 plays up\n\n";
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(1400, 1400);
+    glutInitWindowSize(700, 1150);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Problem 7 - Mixed Strategy Nash Equlibrium");
     initRendering();
